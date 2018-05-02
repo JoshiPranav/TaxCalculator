316e07813dabf5a51955a4c2020a78b39bf34dc9
JoshiPranav <joshpranav@gmail.com>
2018-05-02 13:41:34 +1000

    reactive form onchanges

--- src/app/incomedetailsform/incomedetailsform.component.ts
+++ src/app/incomedetailsform/incomedetailsform.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IncomeDetails } from '../core/models/income-details.model';
 import { TaxDetails } from '../core/models/tax-details.model';
 import { TaxCalculatorService } from '../core/services/tax-calculator.service';
 import { PatternValidator } from '../core/validators/pattern.validator';
-import * as _ from 'lodash';
 import { NumericRangeValidator } from '../core/validators/numeric-range.validator';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-incomedetailsform',
   templateUrl: './incomedetailsform.component.html',
   styleUrls: ['./incomedetailsform.component.css']
 })
-export class IncomedetailsformComponent implements OnInit {
+export class IncomedetailsformComponent implements OnInit, OnChanges {
 
   idForm: FormGroup;
   grossSalary: number;
@@ -34,8 +34,18 @@ export class IncomedetailsformComponent implements OnInit {
       includesSuper : '',
       superannuation : ['', [NumericRangeValidator(9.49, 100)]]
     });
+    this.ngOnChanges();
+  }
+
+  ngOnChanges(): void {
+    this.idForm.valueChanges.subscribe(val => {
+      if (this.idForm.valid) {
+        this.calculate();
+      }
+    });
   }
 
   calculate() {
+    console.log(this.idForm.value);
   }
 }
